5917148349d109b050bede1b6409fb86ddf84380
Tyby <tibofenyvesi84@gmail.com>
2018-02-15 11:13:33 +0000

    The sticky-header and smooth-scroll functionalities have been completed!

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,12 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonials'), '60%');
+var stickyHeader = new StickyHeader();
+
 
