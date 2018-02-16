5d9f52765b1312daf70c447b9200fb5d5f65a1e3
Tyby <tibofenyvesi84@gmail.com>
2018-02-16 13:29:31 +0000

    Modal has been created and functionelisedgit add -A

---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,37 @@
+import $ from 'jquery';
+
+class Modal {
+	constructor() {
+		this.openModalButton = $('.open-modal');
+		this.modal = $('.modal');
+		this.closeModalButton = $('.modal__close');
+		this.events();
+	}
+	
+	events() {
+		// Clicking the open modal button
+		this.openModalButton.click(this.openModal.bind(this));
+		
+		//Clicking the X close modal button
+		this.closeModalButton.click(this.closeModal.bind(this));
+		
+		//Pushes any key to close
+		$(document).keyup(this.keyPressHandler.bind(this));
+	}
+		
+	openModal() {
+		this.modal.addClass('modal--is-visible');
+		return false;
+	}
+	closeModal() {
+		this.modal.removeClass('modal--is-visible');
+	}
+	keyPressHandler(e) {
+		if(e.keyCode == 27) {
+			this.closeModal();
+		}
+	}
+}
+
+export default Modal;
+
